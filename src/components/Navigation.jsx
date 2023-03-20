62faaf7e99a280a61099604b2a695df252026b2d
fortu038 <fortu038@umn.edu>
2023-03-20 11:42:38 -0500

    Modified routing and assembled bones of site

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,13 +1,44 @@
 import React from 'react';
 import { Container, Navbar, Nav } from 'react-bootstrap';
-import { Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.css';
 
-const site_name = "Langevin Heating and Venting";
-const url_name = site_name.replace(/\s/g, "");
+const site_name = "Langevin Heating And Venting";
 
 function Navigation(props) {
+  return(
+    <>
+      <Navbar collapseOnSelect expand="lg">
+        <Container className="text-center">
+          <Navbar.Brand>
+            <h1 className="text-secondary">{site_name}</h1>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="me-auto d-flex align-items-center">
+                <Nav.Link as={Link} to="">
+                  <h5>Home</h5>
+                </Nav.Link>
+                <Nav.Link as={Link} to="about-us">
+                  <h5>About Us</h5>
+                </Nav.Link>
+                <Nav.Link as={Link} to="our-services">
+                  <h5>Our Services</h5>
+                </Nav.Link>
+                <Nav.Link as={Link} to="areas-served">
+                  <h5>Areas Served</h5>
+                </Nav.Link>
+                <Nav.Link as={Link} to="contact-us">
+                  <h5>Contact Us</h5>
+                </Nav.Link>
+              </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
 
+      <Outlet />
+    </>
+  )
 }
 
 export default Navigation;
--- src/components/Routing.jsx
+++ src/components/Routing.jsx
@@ -1,16 +1,30 @@
 import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 
-import PagesRouting from "./PagesRouting";
+import Navigation from "./Navigation";
+import Homepage from "./pages/homepage/Homepage";
+import AboutUs from "./pages/about-us/AboutUs";
+import AreasServed from "./pages/area-served/AreasServed";
+import ContactUs from "./pages/contact-us/ContactUs";
+import OurServices from "./pages/our-services/OurServices";
 
-const site_name = "Langevin Heating and Venting";
+const site_name = "Langevin Heating And Venting";
 const url_name = site_name.replace(/\s/g, "");
 
 function Routing(props) {
-  <Routes>
-    <Route path={`/${url_name}`} element={<PagesRouting />}/>
-    <Route path="*" element={<Navigate to={`/${url_name}`}/>} />
-  </Routes>
+  return(
+    <Routes>
+      <Route path={`/${url_name}`} element={<Navigation />}>
+        <Route index element={<Homepage />}/>
+        <Route path="about-us" element={<AboutUs />}/>
+        <Route path="areas-served" element={<AreasServed />}/>
+        <Route path="contact-us" element={<ContactUs />}/>
+        <Route path="our-services" element={<OurServices />}/>
+        <Route path="*" element={<Navigate to=""/>} />
+      </Route>
+      <Route path="*" element={<Navigate to={`/${url_name}`}/>} />
+    </Routes>
+  )
 }
 
 export default Routing;
