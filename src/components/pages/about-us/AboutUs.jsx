db3e771f7bd32c0a0d8b8893b23120f07e480b19
fortu038 <fortu038@umn.edu>
2023-03-21 16:54:26 -0500

    Updated styling for AboutUs and AreasServed.

--- src/components/pages/about-us/AboutUs.jsx
+++ src/components/pages/about-us/AboutUs.jsx
@@ -1,14 +1,48 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
+
+const worker_img = require("../../../assets/images/worker_placeholder.jpg");
+const job_img = require("../../../assets/images/card_placeholder_image.jpg");
 
 function AboutUs() {
   return(
     <Container className="text-secondary mt-4">
-      <div className="text-secondary d-flex flex-column justify-content-center align-items-center">
+      <Row>
+        <Col>
+          <h3 className="text-center">
+            About Us
+          </h3>
+        </Col>
+      </Row>
+      <Row>
+        <Col className="d-flex justify-content-center mt-4">
+          <img src={worker_img} alt="The founder of Lagevin Heating and Venting." />
+        </Col>
+      </Row>
+      <Row>
+        <Col className="d-flex justify-content-center mt-4">
+          <h5>Our founder and employees work hard to bring the community quality heating and venting.</h5>
+        </Col>
+      </Row>
+      <Row className="my-5">
+        <Col className="d-flex justify-content-center">
+          <img src={job_img} alt="An employee on the job." />
+        </Col>
+        <Col className="d-flex justify-content-center align-items-center">
+          <h5>We work not just with heating, but also with ventilation!</h5>
+        </Col>
+      </Row>
+      {/* <div className="text-secondary d-flex flex-column justify-content-center align-items-center">
         <h3 className="text-center">
           About Us
         </h3>
       </div>
+      <div className="d-flex justify-content-center">
+        <img src={worker_img} alt="The founder of Lagevin Heating and Venting." />
+      </div>
+      <div className="d-flex justify-content-center">
+        <h5>Our founder and employees work hard to bring the community quality heating and venting.</h5>
+      </div> */}
     </Container>
   )
 }
--- src/components/pages/area-served/AreasServed.jsx
+++ src/components/pages/area-served/AreasServed.jsx
@@ -1,14 +1,30 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Card } from 'react-bootstrap';
 
 function AreasServed() {
   return(
     <Container className="text-secondary mt-4">
-      <div className="text-secondary d-flex flex-column justify-content-center align-items-center">
-        <h3 className="text-center">
-          This is the areas served page
-        </h3>
-      </div>
+      <h3>Cities and Areas We Service:</h3>
+      <ul id="areas-served-list">
+        <li>Lindstrom</li>
+        <li>Stacy</li>
+        <li>Forest Lake</li>
+        <li>Chisago City</li>
+        <li>Harris</li>
+        <li>St Croix Falls</li>
+      </ul>
+
+      <br />
+      <br />
+      
+      <Card border="secondary">
+        <Card.Header>Not Sure If You're In Our Service Area?</Card.Header>
+        <Card.Body>
+          <Card.Text>
+            Contact us if you're not sure and we'll get back to you.
+          </Card.Text>
+        </Card.Body>
+      </Card>
     </Container>
   )
 }
